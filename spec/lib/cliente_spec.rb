0efe25d84cddc27718c3e9408887beec06cb5479
Ricardo Malafaia <rbm4@cin.ufpe.br>
2016-05-09 00:40:08 +0000

    Implementacao TDD

--- spec/lib/cliente_spec.rb
+++ spec/lib/cliente_spec.rb
@@ -5,27 +5,26 @@ describe Cliente do
     regex_nome = /[A-Za-z]/
     regex_endereco = /./
     valid_input = "[a-zA-z ]" #
-    
-    self=cliente.new
+    obj = Cliente.new
     
     it "Checar instância de self" do
-        expect(self).to be_an_instance_of(Cliente)
+        expect(obj.to be_an_instance_of(Cliente)
     end
     
     it "O nome é válido?" do
-        expect(self.nome).to match(valid_input) and not_to match(regex_num) #verifica se não tem números, verifica se tem espaço e letras
+        expect(obj.nome).to match(valid_input) and not_to match(regex_num) #verifica se não tem números, verifica se tem espaço e letras
     end
     
     it "endereco é valido?" do
-        expect(self.endereco).to match(regex_endereco)
-        expect(self.endereco).not_to match(" ")
-        expect(self.endereco).not_to be_empty
-        expect(self.endereco).to be_an_instance_of(String)
+        expect(obj.endereco).to match(regex_endereco)
+        expect(obj.endereco).not_to match(" ")
+        expect(obj.endereco).not_to be_empty
+        expect(obj.endereco).to be_an_instance_of(String)
     end
     
     it "telefone só tem numeros?" do
-        expect(us.password).to match(regex_number)
-        expect(us.password).not_to be_empty
-        expect(us.password).not_to match("00000000")
+        expect(obj.telefone).to match(regex_number)
+        expect(obj.telefone).not_to be_empty
+        expect(obj.telefone).not_to match("00000000")
     end
 end
